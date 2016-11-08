208bd73be7bf99ba6071d41e0af4c162d0624d10
David Fischer <djfische@gmail.com>
2016-11-07 21:34:00 -0800

    Add continuous testing and flake8

--- pycoreutils/utils.py
+++ pycoreutils/utils.py
@@ -1,10 +1,8 @@
 import functools
-import glob
 import hashlib
 import os
 import signal
 import stat
-import sys
 
 
 def getsignals():
@@ -119,21 +117,9 @@ def mode2string(mode):
     return s
 
 
-def hasher(algorithm, p):
-    '''
-    :param algorithm: hash algorithm
-    :param p: ArgumentParser
-    '''
+def hasher(algorithm, fd):
     def myhash(args):
-        for fd in filelist2fds(args.FILE, 'rb'):
-            hasj = hashlib.new(algorithm)
-            for data in iter(functools.partial(fd.read, 128), b''):
-                hasj.update(data)
-            print(hasj.hexdigest() + '  ' + fd.name)
-
-    p.set_defaults(func=myhash)
-    p.description = "Print or check {0} ".format(algorithm.upper()) +\
-                    "checksums. With no FILE, or when FILE is -, read " +\
-                    "standard input."
-    p.add_argument('FILE', nargs='*')
-    return p
+        h = hashlib.new(algorithm)
+        for data in iter(functools.partial(fd.read, 4096), b''):
+            h.update(data)
+        return h.hexdigest()
